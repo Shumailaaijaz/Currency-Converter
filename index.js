59fbb81f06b6733221a4e9545d3c018e9c770edc
Your Name <your.email@example.com>
2024-04-06 03:37:32 +0500

    This is currency converter

---
+++ index.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, // Base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let user_anser = await inquirer.prompt([
+    {
+        name: "from",
+        message: " Enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "to",
+        message: " Enter to currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: " Enter from Amount",
+        type: "number",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+]);
+console.log(user_anser);
+let fromAmount = currency[user_anser.from];
+let toAmount = currency[user_anser.to];
+let amount = user_anser.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
---
+++ index.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+
+const currency : any = {
+  USD: 1, // Base currency
+  EUR: 0.91,
+  GBP: 0.76,
+  INR: 74.57,
+  PKR: 280,
+};
+
+        let user_anser = await inquirer.prompt(
+
+[
+    {
+        name: "from",
+        message: " Enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+      },
+      {
+        name: "to",
+        message: " Enter to currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+      },
+      {
+        name: "amount",
+        message: " Enter from Amount",
+        type: "number",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+      },
+]
+   
+);
+
+
+let fromAmount = currency[user_anser.from];
+let toAmount = currency[user_anser.to];
+let amount = user_anser.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
+
+
